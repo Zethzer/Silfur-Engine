b66fc4027d950a91e60ac540fe06df61319372d6
Zethzer <bernard.y24@gmail.com>
2020-04-27 04:31:52 +0200

    Finish handling debug. (#7)

--- Silfur/src/graphics/vulkan/utils/Debug.hpp
+++ Silfur/src/graphics/vulkan/utils/Debug.hpp
@@ -9,58 +9,28 @@ namespace Silfur
 {
     namespace Vk
     {
-        VkResult CreateDebugUtilsMessengerEXT(VkInstance p_instance,
-            const VkDebugUtilsMessengerCreateInfoEXT* p_pCreateInfo,
-            const VkAllocationCallbacks* p_pAllocator,
-            VkDebugUtilsMessengerEXT* p_pDebugMessenger)
-        {
-            auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(p_instance, "vkCreateDebugUtilsMessengerEXT");
-            if (func != nullptr)
-            {
-                return func(p_instance, p_pCreateInfo, p_pAllocator, p_pDebugMessenger);
-            }
-            else
-            {
-                return VK_ERROR_EXTENSION_NOT_PRESENT;
-            }
-        }
-
-        void DestroyDebugUtilsMessengerEXT(VkInstance p_instance,
-            VkDebugUtilsMessengerEXT p_debugMessenger,
-            const VkAllocationCallbacks* p_pAllocator)
-        {
-            auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(p_instance, "vkDestroyDebugUtilsMessengerEXT");
-            if (func != nullptr)
-            {
-                func(p_instance, p_debugMessenger, p_pAllocator);
-            }
-        }
+        inline VkResult CreateDebugUtilsMessengerEXT(VkInstance p_instance, const VkDebugUtilsMessengerCreateInfoEXT* p_pCreateInfo,
+            const VkAllocationCallbacks* p_pAllocator, VkDebugUtilsMessengerEXT* p_pDebugMessenger);
+        inline void DestroyDebugUtilsMessengerEXT(VkInstance p_instance, VkDebugUtilsMessengerEXT p_debugMessenger,
+            const VkAllocationCallbacks* p_pAllocator);
+        inline void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& p_createInfo);
+        inline void displayMessage(VkDebugUtilsMessageSeverityFlagBitsEXT p_messageSeverity,
+            VkDebugUtilsMessageTypeFlagsEXT p_messageType,
+            const char* p_message);
 
         static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
-            VkDebugUtilsMessageSeverityFlagBitsEXT p_MessageSeverity,
-            VkDebugUtilsMessageTypeFlagsEXT p_MessageType,
+            VkDebugUtilsMessageSeverityFlagBitsEXT p_messageSeverity,
+            VkDebugUtilsMessageTypeFlagsEXT p_messageType,
             const VkDebugUtilsMessengerCallbackDataEXT* p_pCallbackData,
             void* p_pUserData)
         {
-            std::cerr << "Validation layer: " << p_pCallbackData->pMessage << std::endl;
+            displayMessage(p_messageSeverity, p_messageType, p_pCallbackData->pMessage);
 
             return VK_FALSE;
         }
-
-        void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& p_createInfo)
-        {
-            p_createInfo = {};
-            p_createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-            p_createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
-                | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
-                | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-            p_createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
-                | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
-                | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-            p_createInfo.pfnUserCallback = debugCallback;
-            p_createInfo.pUserData = nullptr; // Optional
-        }
     }
 }
+
+#include "graphics/vulkan/utils/Debug.inl"
 
 #endif // __SILFUR_GRAPHICS_VULKAN_UTILS_DEBUG_HPP__
