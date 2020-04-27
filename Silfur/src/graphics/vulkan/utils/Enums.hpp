f9bdb6d498ec32cf6b7c3fd7d841c51e31fca1f6
Zethzer <bernard.y24@gmail.com>
2020-04-27 04:26:20 +0200

    Delete tests

--- Silfur/src/graphics/vulkan/utils/Enums.hpp
+++ Silfur/src/graphics/vulkan/utils/Enums.hpp
@@ -9,20 +9,7 @@ namespace Silfur
 {
     namespace Vk
     {
-        enum class DebugUtilsMessageSeverity : VkDebugUtilsMessageSeverityFlagsEXT
-        {
-            VERBOSE = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT,
-            INFO = VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT,
-            WARNING = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT,
-            ERROR = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT
-        } DebugUtilsMessageSeverity;
-
-        enum class VkDebugUtilsMessageType : VkDebugUtilsMessageTypeFlagsEXT
-        {
-            GENERAL = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT,
-            VALIDATION = VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT,
-            PERFORMANCE = VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT
-        } VkDebugUtilsMessageType;
+        // #TODO-Zeth : Implement enum class
     }
 }
 
